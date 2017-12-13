814e63673accba0190f01c1705c5979dd266849e
cellis9421 <cellis9421@gmail.com>
2017-12-12 19:29:11 -0500

    Added tank animation, multiple tanks, tank object

--- public/client.js
+++ public/client.js
@@ -23,21 +23,24 @@ var bgCanvas; // background, static canvas layer
 var ctx; //ctx for gameCanvas
 var bgCtx; //ctx for bgCanvas
 
-/* GAME VARS */
-var gameInterval;
-var tank = {
-	x : 100,
-	y : 100,
-	speed : 2,  //this is in terms of px * FPS per now.
-	image: null
-};
-
 /* IMAGE VARS */
 var dirtImage = new Image();
 dirtImage.src = 'public/img/dirt.png';
-var tankImage = new Image();
-tankImage.src = 'public/img/tank/1.png'; //TODO: transparency
-tank.image = tankImage;
+var tankImage = [new Image(),new Image(),new Image(),new Image(),new Image(),new Image(),new Image(),new Image()];
+tankImage.forEach(function(item, index, array){
+	item.src = 'public/img/tank/' + (index+1) + '.png';
+	console.log(item.src)
+});
+
+/* GAME VARS */
+var gameInterval;
+var playerTank = new Tank(0,0,0,2);
+
+var tanks = [ playerTank,
+	new Tank(1,1000,100,2),
+	new Tank(2,500,150,2),
+	new Tank(3,200,200,2)
+];
 
 /* Document is Ready */
 $(function() { 
@@ -49,6 +52,8 @@ $(function() {
 	bgCtx = bgCanvas.getContext('2d');
 
 	renderBG();
+	ctx.font = '24px serif';
+	
 	setInterval(gameLoop, 1000 / TARGET_FPS); //very inefficient I'd imagine
 });
 
@@ -82,33 +87,65 @@ function render(){
 	if (!ctx) //game ctx not yet retrieved
 		return;
 	ctx.clearRect(0,0,ARENA_WIDTH,ARENA_HEIGHT);
-	ctx.drawImage(tank.image, tank.x, tank.y);
+	
+	/* Draw All Tanks In Game*/
+	
+	tanks.forEach(function(tank, index, array) {
+		ctx.drawImage(tankImage[tank.animPos], tank.x, tank.y);
+		ctx.fillText('Player ' + tank.id, tank.x, tank.y-10);
+	});
 }
 
 /* Update Local Game State */
 function update(){
 	/* Handle Tank Movement */
-
-	var speed = tank.speed;
+	var speed = playerTank.speed;
 	if ((Keys.isDown(Keys.UP) || Keys.isDown(Keys.DOWN)) && 
 		(Keys.isDown(Keys.LEFT) || Keys.isDown(Keys.RIGHT))){
 		speed *= DIAG_SPEED_FACTOR; //Ensure Diag movement doesn't increase speed
 	}
 
-	if (Keys.isDown(Keys.UP)) tank.y-=tank.speed;
-	if (Keys.isDown(Keys.LEFT)) tank.x-=tank.speed;
-	if (Keys.isDown(Keys.DOWN)) tank.y+=tank.speed;
-	if (Keys.isDown(Keys.RIGHT)) tank.x+=tank.speed;
+	if (Keys.isDown(Keys.UP)) { 
+		playerTank.y-= speed; 
+		if(playerTank.animPos < 7 )
+			playerTank.animPos += 1;
+		else
+			playerTank.animPos = 0;
+	}
+	if (Keys.isDown(Keys.DOWN)){ 
+		playerTank.y+= speed; 
+		if(playerTank.animPos >= 1 )
+			playerTank.animPos -= 1;
+		else
+			playerTank.animPos = 7;
+	}
+	
+	if (Keys.isDown(Keys.LEFT)) playerTank.x-= speed;
+	if (Keys.isDown(Keys.RIGHT)) playerTank.x+= speed;
+}
+
+/* Tank Constructor */
+function Tank(id, x, y, speed){
+	this.id = id; //unique id TO-DO: avoid id collision
+	this.x = x;
+	this.y = y;
+	this.speed = (speed < 0 ? 0 : speed); //assure positive integer. this is in terms of px * FPS per now.
+	this.animPos = 0; //Animation position
+}
+
+Tank.prototype.move = function(x,y){
+	this.x = x;
+	this.y = y;
 }
 
 /* Keep Track of Keys Pressed */
 var Keys = {
 	_pressed: {},
 
-	UP: 87, //W
-	LEFT: 65, //A
+	UP: 87,   // W
+	LEFT: 65, // A
 	DOWN: 83, // S
-	RIGHT: 68, //D
+	RIGHT: 68,// D
   
 	isDown: function(keyCode) {
 		return this._pressed[keyCode];
